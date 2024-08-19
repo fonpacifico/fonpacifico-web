deac28a4464d7e0b2300d9721e3b3c275538a148
Cuadros <juan.josecuadros1@gmail.com>
2024-08-18 21:15:14 -0500

    finished programas y cursos

---
+++ src/content/programasYCursosView.js
@@ -0,0 +1,67 @@
+export const programs = [
+	{
+		imgRoute: "/imagenes_programas_y_cursos/image.jpg",
+		title: "Alianza Paz Pacifico",
+		body: [
+			"Programa de interés público para el fomento de cierre de brechas socioeconómicas y el fortalecimiento de capacidades que beneficien a los Municipios y Departamentos del país, contribuyendo a superar inequidades, y los factores sociales que incentivan la violencia y perpetúan la pobreza, facilitando la unión de esfuerzos del sector privado y público de las regiones.",
+			"Líneas de acción del programa: Infraestructura, programas sociales, culturales, económicos, ambientales y fortalecimiento institucional.",
+		],
+		ctas: [],
+	},
+	{
+		imgRoute: "/imagenes_programas_y_cursos/image-1.jpg",
+		title: "Tejiendo Vínculos",
+		body: [
+			"Programa para la promoción de  la inteligencia emocional, Pautas de Crianza y Habilidades Para la Vida a través de procesos pedagógicos, campañas de comunicación y movilización social contribuyendo a la construcción de imaginarios colectivos de una sociedad incluyente, próspera, en paz y mentalmente sana.",
+		],
+		ctas: [
+			{
+				text: "Ir al sitio web",
+				link: "#",
+			},
+			{
+				icon: "/icons/facebook.svg",
+				text: "Facebook",
+				link: "#",
+			},
+			{
+				icon: "/icons/youtube.svg",
+				text: "Youtube",
+				link: "#",
+			},
+		],
+	},
+	{
+		imgRoute: "/imagenes_programas_y_cursos/image-2.jpg",
+		title: "Redes Lab",
+		body: [
+			"Es una iniciativa de innovación social transformativa en Laboratorios de Paz. Se centra no sólo en las personas, sino también en los ecosistemas de desarrollo humano. Su objetivo es generar capacidad de respuesta social local y formar nodos como Agencias de Desarrollo Local, basados en tecnología, redes de beneficiarios y fondos fiduciarios que promuevan la sostenibilidad, la autonomía y la equidad transgeneracional.",
+			"Misión: Promover la articulación y fortalecimiento de las estructuras, actores y ecosistemas locales que materializan la paz y el desarrollo sostenible en grupos y territorios vulnerables afectados por la pobreza y la violencia. Movilizar recursos, conexiones, tecnologías y conocimientos para expandir la estructura de oportunidades y la capacidad de agencia local, dotándolos de activos sociales y herramientas (financieras y no financieras); fomentando la autogestión de las problemáticas institucionales, económicas y sociales.",
+		],
+	},
+	{
+		imgRoute: "/imagenes_programas_y_cursos/image-3.jpg",
+		title: "Afro Colombian Channel",
+		body: [
+			"El fondo mixto de etnocultura y desarrollo social Fonpacifico se une a la iniciativa nacional de construcción de la paz y el desarrollo social de las comunidades a través de su política pública alianza paz pacifico y de su programa tejiendo vínculos. En el marco de su laboratorio de comunicaciones, Fonpacifico presenta su canal Afro Colombian Channel, un canal desarrollado para potenciar la diversidad étnica regional, visibilizar sus iniciativas culturales y aportar a su desarrollo.",
+		],
+		ctas: [
+			{
+				text: "Ir al sitio web",
+				link: "#",
+			},
+			{
+				icon: "/icons/youtube.svg",
+				text: "Youtube",
+				link: "#",
+			},
+		],
+	},
+	{
+		imgRoute: "/imagenes_programas_y_cursos/image-4.jpg",
+		title: "Cursos Fonpacífico",
+		body: [
+			"Creemos firmemente en el poder transformador de la educación para cambiar vidas y revitalizar comunidades enteras. Nuestra misión es construir puentes hacia un futuro mejor, donde la inclusión, la diversidad y la igualdad de oportunidades no sean solo aspiraciones, sino una realidad tangible para todos.",
+		],
+	},
+];
--- src/views/ProgramasYCursosView.vue
+++ src/views/ProgramasYCursosView.vue
@@ -1,7 +1,161 @@
-<script setup></script>
+<script setup>
+import { programs } from '@/content/programasYCursosView.js';
+</script>
 
 <template>
-  <div>Programas y cursos</div>
+  <figure class="hero__image">
+    <img
+      src="/hero-programas-y-cursos.jpeg"
+      alt=""
+    />
+  </figure>
+  <section class="heading fp-container fp-padded">
+    <h1>Programas y cursos</h1>
+    <p>
+      En Fonpacífico nos esforzamos por devolverle a la comunidad tanto como sea
+      posible. Para lo cual, hemos creado varios programas y cursos con el
+      objetivo de fortalecer la calidad de vida colombiana.
+    </p>
+  </section>
+  <section class="programs fp-container fp-padded-bottom">
+    <article
+      v-for="program in programs"
+      class="program"
+    >
+      <figure class="program-image">
+        <img
+          :src="program.imgRoute"
+          alt=""
+        />
+      </figure>
+      <div class="program-copy">
+        <h3>{{ program.title }}</h3>
+        <p v-if="program.body.length === 1">
+          {{ program.body[0] }}
+        </p>
+        <p
+          v-else-if="program.body.length > 1"
+          v-for="paragraph in program.body"
+          :key="paragraph"
+        >
+          {{ paragraph }}
+        </p>
+        <div class="program-buttons">
+          <a
+            class="program-button button button--secondary"
+            v-for="link in program.ctas"
+            :key="link.text"
+          >
+            <span v-if="link.icon">
+              <img
+                :src="link.icon"
+                alt=""
+              />
+            </span>
+            {{ link.text }}
+          </a>
+        </div>
+      </div>
+    </article>
+  </section>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use '@/sass/abstracts' as *;
+
+.hero {
+  &__image {
+    width: 100%;
+    border-radius: 2rem;
+
+    @include breakpoint-min('md') {
+      aspect-ratio: 2.4 / 1;
+      max-height: 600px;
+    }
+
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center;
+    }
+  }
+}
+
+.heading {
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 2rem;
+
+  @include breakpoint-min('md') {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+
+    h1 {
+      grid-column: 1;
+    }
+
+    p {
+      grid-column: 2;
+    }
+  }
+}
+
+.programs {
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 4rem;
+
+  @include breakpoint-min('md') {
+    gap: 8rem;
+  }
+
+  .program {
+    display: flex;
+    flex-flow: column nowrap;
+    place-items: center;
+    gap: 2rem;
+
+    @include breakpoint-min('md') {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      row-gap: 8rem;
+    }
+
+    &-image {
+      width: 100%;
+      border-radius: 2rem;
+
+      img {
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        aspect-ratio: 44 / 25;
+
+        @include breakpoint-min('md') {
+          aspect-ratio: unset;
+        }
+      }
+    }
+
+    &-copy {
+      grid-column: 2;
+      display: flex;
+      flex-flow: column nowrap;
+      gap: 1rem;
+
+      @include breakpoint-min('md') {
+        gap: 1.5rem;
+      }
+    }
+
+    &-buttons {
+      display: flex;
+      flex-flow: row wrap;
+      gap: 1rem;
+    }
+  }
+}
+</style>
