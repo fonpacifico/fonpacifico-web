7e2cbd5a3428f4b9839b4d813885a6ce1481ea6e
Cuadros <juan.josecuadros1@gmail.com>
2024-09-16 22:48:09 -0500

    convocatorias'

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import { router } from "./server";
 import { createPinia } from "pinia";
 
 import fadeIn from "./directives/fadeIn";
+import Primevue from "primevue/config";
 import App from "./App.vue";
 
 import "./sass/main.scss";
 
 createApp(App)
 	.directive("fade-in", fadeIn)
+	.use(Primevue, {
+		unstyle: true,
+	})
 	.use(createPinia())
 	.use(router)
 	.mount("#app");
--- src/views/ConvocatoriasView.vue
+++ src/views/ConvocatoriasView.vue
@@ -1,13 +1,46 @@
 <script setup>
-import CarouselSection from '@/globals/CarouselSection.vue';
-import CopyBlock from '@/globals/CopyBlock.vue';
+import { onMounted, ref } from 'vue';
 
-import ProgramasInteres from '@/components/ProgramasInteres.vue';
-import StatisticsSection from '@/components/StatisticsSection.vue';
-import NewsLetter from '@/components/NewsLetter.vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import InputText from 'primevue/inputtext';
+import { FilterMatchMode } from '@primevue/core/api';
 
-import { carouselItems } from '@/content/nosotrosView';
-import * as content from '@/content/serviciosView';
+const convocatorias = ref();
+const expandedRows = ref({});
+const loading = ref(true);
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+
+  return formatter.format(date);
+};
+
+const fetchConvocatorias = async () => {
+  const response = await fetch(
+    'https://j4hvvf8.localto.net/maestroconvocatoriasx'
+  );
+
+  const data = await response.json();
+
+  return data;
+};
+
+onMounted(async () => {
+  convocatorias.value = await fetchConvocatorias();
+  loading.value = false;
+});
 </script>
 
 <template>
@@ -24,12 +57,88 @@ import * as content from '@/content/serviciosView';
           </p>
         </div>
       </div>
+      <div class="convocatorias__table">
+        <div class="convocatorias__table-heading">
+          <div>
+            <h2>Lista de convocatorias</h2>
+            <p>
+              Puedes buscar o filtrar convocatorias si estás buscando algo
+              específico
+            </p>
+          </div>
+          <div class="icon-wrapper">
+            <icon-field>
+              <input-icon>
+                <span class="material-symbols-outlined">search</span>
+              </input-icon>
+              <input-text
+                v-model="filters['global'].value"
+                placeholder="Buscar"
+              />
+            </icon-field>
+          </div>
+        </div>
+        <data-table
+          :loading="loading"
+          v-model:expandedRows="expandedRows"
+          v-model:filters="filters"
+          filterDisplay="row"
+          :value="convocatorias"
+          paginator
+          :rows="8"
+          dataKey="id_convocatoria"
+          tableStyle="min-width: 60rem"
+        >
+          <Column
+            expander
+            style="width: 0"
+          />
+          <Column
+            field="codigo"
+            header="Código"
+          ></Column>
+          <Column
+            field="estado"
+            header="Estado"
+          ></Column>
+          <Column
+            field="contrato"
+            header="Tipo de contrato"
+          ></Column>
+          <Column
+            field="modoContratacion"
+            header="Contratación"
+          ></Column>
+          <Column
+            sortable
+            field="fecha"
+            header="Fecha"
+          >
+            <template #body="{ data }">{{ formatDate(data.fecha) }}</template>
+          </Column>
+          <template #expansion="slotProps">
+            <p>test content</p>
+          </template>
+        </data-table>
+      </div>
     </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
 @use '../sass/abstracts/_mixins' as *;
+
+.test {
+  background-color: red;
+}
+.icon-wrapper {
+  display: grid;
+  place-content: flex-start;
+
+  @include breakpoint-min('md') {
+    place-content: center;
+  }
+}
 
 .convocatorias {
   display: flex;
@@ -54,6 +163,21 @@ import * as content from '@/content/serviciosView';
 
     @include breakpoint-min('md') {
       grid-column: 2;
+    }
+  }
+
+  &__table-heading {
+    display: flex;
+    flex-flow: column nowrap;
+    gap: 1.5rem;
+
+    h2 {
+      font-size: 1.5rem;
+    }
+
+    @include breakpoint-min('md') {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
     }
   }
 }
